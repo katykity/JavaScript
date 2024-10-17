c70b369a5bc033ef44f31f271100da7dd9f46439
Kathya <katy.nicole.mv14@gmail.com>
2024-10-16 20:47:43 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Hola mundo");
+
+console.log('Hola ' + `Mundo`);
+
+console.log((5+5.555)/2*10);
+
+console.log(parseFloat("5.5") + parseFloat("5.5"));
+
+console.log(true);
+
+console.log(false == 0);
+
+let mensaje = 30;
+
+console.log(typeof numero);
+
+let numeroPrimo = 11;
+
+console.log(numeroPrimo);
+
+const edad = 15;
+
+if (edad >= 18 ) {
+    edadUsuario=`mayor de edad`;
+}else {
+    edadUsuario=`menor de edad`;
+}
+
+console.log(edadUsuario);
